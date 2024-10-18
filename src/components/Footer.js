3eadaf815f5a32182c245edc8157a6cd12508ccd
Anthony <120075541+AnthonysMotion@users.noreply.github.com>
2024-10-19 01:29:41 +1300

    Update Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+// src/components/Footer.js
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer style={{ padding: '10px', textAlign: 'center', background: '#333', color: '#fff', marginTop: '20px' }}>
+      <p>© anth</p>
+    </footer>
+  );
+};
+
+export default Footer;
