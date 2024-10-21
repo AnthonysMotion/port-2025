77026c56ad3179ae681a9d273d7b8b745df78ea9
Anthony <120075541+AnthonysMotion@users.noreply.github.com>
2024-10-22 04:45:26 +1300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -34,7 +34,7 @@ const Home = () => {
         </div>
         <img
           className='parallax-header-bg'
-          src='https://plus.unsplash.com/premium_photo-1728732559227-5ae28c0053f1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+          src='https://images.unsplash.com/photo-1729494130269-c5610ddbd300?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
           alt='Background'
           style={{ transform: `translateY(${offsetY * 0.3}px)` }}
         />
