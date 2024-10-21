d8c7ad2bcecdf698704416501ab43691e12837a4
Anthony <120075541+AnthonysMotion@users.noreply.github.com>
2024-10-22 05:18:51 +1300

    Create smoothscroll.js

---
+++ src/components/smoothscroll.js
@@ -0,0 +1,28 @@
+
+import React, { useEffect } from 'react';
+
+const SmoothScroll = () => {
+  useEffect(() => {
+    const body = document.body;
+    const scrollWrap = document.getElementsByClassName("container")[0];
+    const height = scrollWrap.getBoundingClientRect().height - 1;
+    const speed = 0.1;
+    
+    let offset = 0;
+    
+    body.style.height = Math.floor(height) + "px";
+
+    const smoothScroll = () => {
+      offset += (window.pageYOffset - offset) * speed;
+      const scroll = "translateY(-" + offset + "px) translateZ(0)";
+      scrollWrap.style.transform = scroll;
+      requestAnimationFrame(smoothScroll);
+    };
+
+    smoothScroll();
+  }, []);
+
+  return null; // This component doesn't render anything
+};
+
+export default SmoothScroll;
