8bdc829703dcb3233d0673e291c9e45442f05fef
Anthony <120075541+AnthonysMotion@users.noreply.github.com>
2024-10-22 21:55:05 +1300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Nav from './components/Nav';
-import Footer from './components/Footer';
+//import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
@@ -26,10 +26,10 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-        <Footer />
       </div>
     </Router>
   );
 }
+//<Footer />
 
 export default App;
