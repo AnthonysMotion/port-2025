bd999a1b68836ab51ba4819a21c9f8d516018f6a
Anthony <120075541+AnthonysMotion@users.noreply.github.com>
2024-10-23 00:11:37 +1300

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom';
 import { animateText } from '../components/gsap';
 
 const Contact = () => {
+    useEffect(() => {
+        document.title = "Anthony Thach | Contact";
+    }, []);
 
   return (
     <div className='container'>
