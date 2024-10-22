71c0427c05c646d93d960adbd5198bb94d7937a4
Anthony <120075541+AnthonysMotion@users.noreply.github.com>
2024-10-22 20:46:16 +1300

    Revert "Update Home.js"
    This reverts commit e464bb6c372591785f8af56eae68fceb997eca7b.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,18 @@ import { Link } from 'react-router-dom';
 import { animateText } from '../components/gsap';
 
 const Home = () => {
+  const projects = [
+    { id: 1, title: 'Project 1', imageUrl: 'https://images.unsplash.com/photo-1726853546098-380e29da9e31?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', link: '/project1', type: 'rectangle', section: 'left' },
+    { id: 2, title: 'Project 2', imageUrl: 'https://images.unsplash.com/photo-1726853546098-380e29da9e31?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', link: '/project2', type: 'rectangle', section: 'left' },
+    { id: 3, title: 'Project 3', imageUrl: 'https://images.unsplash.com/photo-1726853546098-380e29da9e31?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', link: '/project3', type: 'square', section: 'middle' },
+    { id: 4, title: 'Project 4', imageUrl: 'https://images.unsplash.com/photo-1726853546098-380e29da9e31?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', link: '/project4', type: 'square', section: 'middle' },
+    { id: 5, title: 'Project 5', imageUrl: 'https://images.unsplash.com/photo-1726853546098-380e29da9e31?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', link: '/project5', type: 'square', section: 'right' },
+  ];
+
+  const leftColumnProjects = projects.filter(project => project.section === 'left');
+  const middleColumnProjects = projects.filter(project => project.section === 'middle');
+  const rightColumnProjects = projects.filter(project => project.section === 'right');
+
   const [offsetY, setOffsetY] = useState(0);
 
   const handleScroll = () => {
