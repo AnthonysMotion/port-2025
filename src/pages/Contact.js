3b5f7ff64300a791df93aa4a213142ad71016cee
Anthony <120075541+AnthonysMotion@users.noreply.github.com>
2024-10-22 23:40:23 +1300

    Update Contact.js

---
+++ src/pages/Contact.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState, useRef } from 'react';
+import { Link } from 'react-router-dom';
+import { animateText } from '../components/gsap';
+
+const Contact = () => {
+
+  return (
+    <div className='container'>
+        <h1>contact</h1>
+    </div>
+  );
+};
+
+export default Contact;
