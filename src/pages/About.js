8fc36904b6b4c7219e095489d9dc0687bea5b094
Anthony <120075541+AnthonysMotion@users.noreply.github.com>
2024-10-22 23:40:18 +1300

    Update About.js

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState, useRef } from 'react';
+import { Link } from 'react-router-dom';
+import { animateText } from '../components/gsap';
+
+const About = () => {
+
+  return (
+    <div className='container'>
+        <h1>about</h1>
+    </div>
+  );
+};
+
+export default About;
