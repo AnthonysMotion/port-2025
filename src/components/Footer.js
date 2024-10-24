988b058be879eb72674a2cdc7b9eb22ed4cfbba4
Anthony <120075541+AnthonysMotion@users.noreply.github.com>
2024-10-25 00:11:20 +1300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,13 @@
 import React from 'react';
 
 const Footer = () => {
+
   return (
     <div className='footer'>
       <footer>
+      <h2>Get in touch — <a href="mailto:hello@anthonythach.com" className='color'>hello@anthonythach.com</a></h2>
+
         <div className='footer-left'>
-          <img src='../logo.svg'></img>
-        </div>
-        <div className='footer-right'>
           <h1>EXCELLENT DESIGN, AT SCALE AND SPEED</h1>
           <p>© ANTHONY THACH 2024</p>
         </div>
