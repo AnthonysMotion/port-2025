692a8b454a3da8a7c83b13f96577af1310100f39
Anthony <120075541+AnthonysMotion@users.noreply.github.com>
2024-10-25 17:18:50 +1300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { HashRouter as Router } from 'react-router-dom';
 import { gsap } from "gsap";
 import { TextPlugin } from "gsap/TextPlugin";
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Nav from './components/Nav';
 import CustomCursor from './components/cursor';
 import AnimatedRoutes from './components/AnimatedRoutes';
 
-gsap.registerPlugin(TextPlugin, ScrollTrigger);
+gsap.registerPlugin(TextPlugin);
+gsap.registerPlugin(ScrollTrigger);
 
 function App() {
   return (
