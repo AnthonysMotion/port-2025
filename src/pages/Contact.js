2e57dd09c977dd30237bcef96851e2bf319edf72
Anthony <120075541+AnthonysMotion@users.noreply.github.com>
2024-10-25 20:38:11 +1300

    Style contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import emailjs from 'emailjs-com';
 
+import Footer from '../components/Footer';
+
 const Contact = () => {
   useEffect(() => {
     document.title = "Anthony Thach | Contact";
   }, []);
 
   const [formData, setFormData] = useState({
-    from_name: '',  // Change this key
-    email: '',      // Keep this for reference, but include in the message
+    from_name: '',
+    email: '',
     message: ''
   });
 
@@ -51,7 +53,7 @@ const Contact = () => {
 
     emailjs.send(serviceID, templateID, templateParams, publicKey)
       .then(() => {
-        setSuccessMessage('Your message has been sent successfully!');
+        setSuccessMessage('Message sent, thank you!');
         setFormData({ from_name: '', email: '', message: '' });
         setErrorMessage('');
       })
@@ -66,48 +68,68 @@ const Contact = () => {
 
   return (
     <div className='container'>
-      <div className='form-container'>
-        <h1>Contact</h1>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor='from_name'>Name:</label>
-            <input
-              type='text'
-              id='from_name'
-              name='from_name'
-              value={formData.from_name}
-              onChange={handleChange}
-              required
-            />
+      <div className='form-wrapper'>
+        <div className='form-header'>
+          <h1>Let's work together</h1>
+          <p>If you have a potential project and think I'd be the ideal partner to bring it to life, please get in touch at <a href="mailto:hello@anthonythach.com" className='color email-hover'>hello@anthonythach.com</a>.<br /><br />I'd love to hear from you!<br /><br /><br /><br /></p>
+          <p>Alternatively, you may contact me over at:</p>
+          <div className="contact-social-icons">
+            <a href="https://x.com/anthonysmotion" target="_blank" rel="noopener noreferrer">
+              <i className="fa-brands fa-x-twitter"></i>
+            </a>
+            <a href="https://www.instagram.com/anthonysmotion/" target="_blank" rel="noopener noreferrer">
+              <i className="fa-brands fa-instagram"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/anthonythach/" target="_blank" rel="noopener noreferrer">
+              <i className="fa-brands fa-linkedin"></i>
+            </a>
           </div>
-          <div>
-            <label htmlFor='email'>Email:</label>
-            <input
-              type='email'
-              id='email'
-              name='email'
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor='message'>Message:</label>
-            <textarea
-              id='message'
-              name='message'
-              value={formData.message}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <button type='submit' disabled={isSending}>
-            {isSending ? 'Sending...' : 'Send Message'}
-          </button>
-        </form>
-        {successMessage && <p style={{ color: 'green' }}>{successMessage}</p>}
-        {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+        </div>
+        <div className='form-container'>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor='from_name'>Name:</label>
+              <input
+                type='text'
+                id='from_name'
+                name='from_name'
+                value={formData.from_name}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor='email'>Email:</label>
+              <input
+                type='email'
+                id='email'
+                name='email'
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor='message'>Message:</label>
+              <textarea
+                id='message'
+                name='message'
+                value={formData.message}
+                onChange={handleChange}
+                required
+                style={{ height: '10rem' }}
+              />
+            </div>
+            <button type='submit' disabled={isSending}>
+              {isSending ? 'Sending...' : 'SUBMIT'}
+            </button>
+          </form>
+          {successMessage && <p style={{ color: '#dcf263' }}>{successMessage}</p>}
+          {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+        </div>
       </div>
+
+      <Footer />
     </div>
   );
 };
