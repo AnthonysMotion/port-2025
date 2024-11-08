64342397d78067413da98a97093e838269cd3d15
Anthony <120075541+AnthonysMotion@users.noreply.github.com>
2024-11-08 18:03:29 +1300

    Fix missing images & mobile responsiveness

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,11 +6,12 @@ import Footer from '../components/Footer';
 
 const Home = () => {
   const projects = [
-    { id: 6, title: 'Photography Portfolio', type: 'Photography', brand: 'Miscellaneous', hoverImageUrl: `${process.env.PUBLIC_URL}/img/6.png`, link: '/photography' },
-    { id: 0, title: 'Lenovo Hero Stories', type: 'Social Media', brand: 'Lenovo', hoverImageUrl: `${process.env.PUBLIC_URL}/img/0.gif`, link: '/lenovo' },
-    { id: 1, title: 'Nova Esports', type: 'Brand Direction', brand: 'Nova Esports', hoverImageUrl: `${process.env.PUBLIC_URL}/img/1.gif`, link: '/nova-esports' },
-    { id: 3, title: 'Discord Advert', type: 'Animation', brand: 'Discord', hoverImageUrl: `${process.env.PUBLIC_URL}/img/3.gif`, link: '/discord' },
-    { id: 4, title: 'Logo Animations', type: 'Animation', brand: 'Miscellaneous', hoverImageUrl: `${process.env.PUBLIC_URL}/img/4.gif`, link: '/logo-animations' },
+    { id: 6, title: 'Photography Portfolio', type: 'Photography', brand: 'Miscellaneous', imageUrl: `${process.env.PUBLIC_URL}/img/6.png`, hoverImageUrl: `${process.env.PUBLIC_URL}/img/projects/0.png`, link: '/photography' },
+    { id: 0, title: 'Lenovo Hero Stories', type: 'Social Media', brand: 'Lenovo', imageUrl: `${process.env.PUBLIC_URL}/img/projects/5.png`, hoverImageUrl: `${process.env.PUBLIC_URL}/img/0.gif`, link: '/lenovo' },
+    { id: 1, title: 'Nova Esports', type: 'Brand Direction', brand: 'Nova Esports', imageUrl: `${process.env.PUBLIC_URL}/img/projects/4.png`, hoverImageUrl: `${process.env.PUBLIC_URL}/img/1.gif`, link: '/nova-esports' },
+    { id: 3, title: 'Discord Advert', type: 'Animation', brand: 'Discord', imageUrl: `${process.env.PUBLIC_URL}/img/projects/3.png`, hoverImageUrl: `${process.env.PUBLIC_URL}/img/3.gif`, link: '/discord' },
+    { id: 4, title: 'Logo Animations', type: 'Animation', brand: 'Miscellaneous', imageUrl: `${process.env.PUBLIC_URL}/img/projects/2.png`, hoverImageUrl: `${process.env.PUBLIC_URL}/img/4.gif`, link: '/logo-animations' },
+    { id: 5, title: 'XP Esports', type: 'Broadcasting', brand: 'XP Esports', imageUrl: `${process.env.PUBLIC_URL}/img/projects/1.png`, hoverImageUrl: `${process.env.PUBLIC_URL}/img/5.gif`, link: '/xpe' }
   ];
 
   const images = [
@@ -220,7 +221,6 @@ const Home = () => {
               onMouseEnter={() => handleMouseEnter(project.id)}
               onMouseLeave={handleMouseLeave}
             >
-              {/* Replaced <a> with <Link> for internal routing */}
               <Link to={project.link} style={{ color: 'inherit', textDecoration: 'none', display: 'flex', width: '100%' }}>
                 <p className="left-text">{project.brand}</p>
                 <h1>{project.title}</h1>
